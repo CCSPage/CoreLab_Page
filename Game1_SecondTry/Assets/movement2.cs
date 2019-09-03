bcb42acb79cfcde43e4707b95669e9bf1c26ca06
Chloe Page <lovechloepuff@gmail.com>
2019-09-03 01:40:52 -0400

    same concept as my first iteration just applied differently

---
+++ Game1_SecondTry/Assets/movement2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class movement2 : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(new Vector2(-5, 0) * Time.deltaTime*speed);
+        if (transform.position.x <= -8)
+        {
+            transform.position = new Vector2(8, transform.position.y);
+        }
+
+    }
+
+}
