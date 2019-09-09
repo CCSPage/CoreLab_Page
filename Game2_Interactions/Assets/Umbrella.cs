2e77eed20a71b4a3ea9e1a9ebba51c606a46b0d0
Chloe Page <lovechloepuff@gmail.com>
2019-09-09 17:36:20 -0400

    final draft of umbrella catching game

---
+++ Game2_Interactions/Assets/Umbrella.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Umbrella : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+  
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        // checking if the left mouse button is pressed
+        if (Input.GetMouseButtonDown(0))
+        {
+            //taking the mouse position and converting to vector3
+            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 direction = worldPoint - transform.position;
+            rb.AddForce(new Vector2(direction.x, direction.y), ForceMode2D.Impulse);
+        }
+
+    }
+}
+
